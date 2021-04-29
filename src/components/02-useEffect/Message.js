666f8bf3b772988bc4036a80ea02de4cd6f820af
Erick2418 <erickjoellimon@outlook.com>
2021-04-28 22:36:23 -0500

    Coords, UseEffect clear

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,18 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
-
     
+    const [coords, setcoords] = useState({ x:0, y:0})
+    const {x,y}=coords;
     useEffect(() => {
-        console.log('componente montado');
+        
+        const mouseMove=(e)=>{
+            const coors={ x:e.x, y:e.y };
+            setcoords(coors);
+        }
+
+        window.addEventListener('mousemove',mouseMove);
         return () => {
-            console.log('Componente desmontado');
+            window.removeEventListener('mousemove',mouseMove);
         }
     }, [])
 
+
+
     return (
         <div>
-            <h2>Eres Genial!!!!</h2>        
+            <h2>Eres Genial!!!!</h2>
+            <p> x:{x}   y:{y}</p>        
         </div>
     );
 
