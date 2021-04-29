339e5caa3dbb5baa624f926e840ab552be40940d
Erick2418 <erickjoellimon@outlook.com>
2021-04-28 22:03:09 -0500

    SimpleForm and useEffect for parameters of state

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,68 @@
+
+import React, { useEffect, useState } from 'react';
+import './effects.css';
+
+export const SimpleForm = () => {
+
+    const [formState, setformState] = useState({
+        name:'',
+        email:''
+    });
+
+    const {name,email}= formState;
+
+    useEffect(() => {/*CADA QUE CAMBIE SE VUELE A EJECUTAR, SI COLOCAS [] SOLO 1 VEZ */
+        console.log('HEY!!!');
+    },[])
+
+    useEffect(() => {
+        console.log('FormState Cambio');
+    },[formState])
+    useEffect(() => {
+        console.log('Email cambio');
+    },[email])
+    
+    const handleInputChange= ({target})=>{
+
+        setformState({...formState,
+            [target.name]:target.value
+        });
+
+    }
+    
+    return (
+        <>
+            <h1>useEffect</h1>
+            <hr/>
+
+            <div className="form-group">
+                <input
+                type="text"
+                name="name"
+                className="form-control"
+                placeholder="Tu nombre"
+                autoComplete="off"
+                value={name}
+                onChange={ handleInputChange }
+                />
+            </div>
+            <div className="form-group">
+                <input
+                type="text"
+                name="email"
+                className="form-control"
+                placeholder="email@gmail.com"
+                autoComplete="off"
+                value={email}
+                onChange={ handleInputChange }
+                />
+            </div>    
+
+
+        </>
+    )
+}
+
+
+
+
