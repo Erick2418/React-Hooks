b9066534e386268ade1311b9024ccec13160e1c4
Erick2418 <erickjoellimon@outlook.com>
2021-05-03 13:13:54 -0500

    Optimizando TodoApp

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,51 +1,25 @@
 import React, { useEffect, useReducer } from 'react'
-import { useForm } from '../../hooks/useForm'
 import "./styles.css"
+import { TodoAdd } from './TodoAdd'
+import { TodoList } from './TodoList'
 import { todoReducer } from './todoReducer'
-// const initialState=[{
-//     id: new Date().getTime(),// genera un numero largo
-//     desc: 'Aprender React',
-//     done: false
-// }]  corte y pegue esto en el init
-// ******* PARA INICIAR POR DEFECTO
-// const init =()=>{
-//     return[{
-//         id: new Date().getTime(),
-//         desc: 'Aprender React',
-//         done: false
-//     }];
-// }
-// ******* PARA INICIAR DEL LOCALSTORAGE
+ 
 const init= () =>{
     return JSON.parse(localStorage.getItem('todos'))|| [];
-    /*Que sucedio aqui? pues... como sabemos que esto regresa string..
-    y si no tiene nada en el localStorage.. pues se moria el programa
-    por ello hacemos la condicion que si no esta disponible nada
-    en el localStorage se regresa un ARREGLO VACIO */
+  
 }
 
 
 export const TodoApp = () => {
 
     const [todos,dispatch] = useReducer(todoReducer, [],init);
-    //const [state, dispatch] = useReducer(reducer, initialState, init)
-    //reemplaze el initianstate por un arreglo vacio
-    const [{description}, handleInputChange, reset] =useForm({
-        description: ''
-    });
-        /*Con esto guardaremos la info en el localStorage.
-        el useEffect permitira que se haga solo cuando hay cambios
-        no acada rato */
+     
+   
     useEffect(() => {
 
        localStorage.setItem('todos',JSON.stringify(todos));
     }, [todos]);
-   /*TAREA   CREEN EL DELETE  
-   LLAMAR LA FUNCION DEL DELETE DEL TODOREDUCER, REGRESA LOS TODO EXCEPTO EL QUE LE DIJE
-   *CREAR LA ACCION
-   *HACER EL DISPACH
-   
-   */
+    
     const handleDelete=(todoId)=>{
        
         const action={
@@ -62,97 +36,33 @@ export const TodoApp = () => {
         }
         dispatch(action);
     }
-
-
-
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-
-        if(description.trim().length<=1){
-            return;
-        }
-
-        const newTodo={
-            id: new Date().getTime(),// genera un numero largo
-            desc: description,
-            done: false
-        };
-
-        const action = {
+ 
+    const handleAddTodo= (newTodo)=>{
+        dispatch({
             type: 'add',
             payload:newTodo
-        }
-
-        dispatch(action);
-
-        reset();
-
+        });
     }
 
-
+ 
     return (
         <div>
             <h1>TodoApp ({todos.length})</h1>
             <hr/>
             <div className="row">
                 <div className="col-7">
-                    <ul className="list-group list-group-flush">
-                    {
-                            todos.map( (todo,i)=>(
-                            <li
-                                key={todo.id}
-                                className="list-group-item"
-                            >
-                            
-                            <p className={`${todo.done && 'complete'}`}
-                            onClick={ ()=>{ handleToogle(todo.id)} }
-                            >{i+1} . {todo.desc} </p>
-                            
-                            <button
-                                className="btn btn-danger"
-                                onClick={
-                                    /*ESTO ES REEE IMPORTANTE
-                                    RECUERDA QUE SI QUIERES EL ID O USAR
-                                    UN ELEMENTO Q SE CREO EN EL .MAP
-                                    TIENES QUE MANDARLO POR FUNCION DE FLECHA
-                                    DE ESTA FORMA NO SE EJECUTA AL INICIO
-                                    SI NO CUANDO LO LLAMES */
-                                    
-                                    ()=>{
-                                    handleDelete(todo.id)
-                                } }
-                            >
-                                    Borrar
-                            </button>
 
-                            </li>
-                            ))
+                    {
+                        <TodoList 
+                        todos={todos} 
+                        handleToogle={handleToogle} 
+                        handleDelete={handleDelete}/>
                     }
-                </ul>
                 </div>
                 <div className="col-5">
-                    <h4>Agregar TODO</h4>
-                    <hr/>
-                    <form onSubmit={handleSubmit}>
-                        <input
-                            type="text"
-                            name="description"
-                            className="form-control"
-                            placeholder="Aprender.."
-                            autoComplete="off"
-                            value={description}
-                            onChange={handleInputChange}
-                        />
-                        
-                        <button 
-                        type="submit"
-                        className="btn btn-outline-primary mt-1 btn-block"
-                        >
-                        Agregar
-                    </button>
-
-                    </form>
-                    
+                <TodoAdd 
+                    handleAddTodo={handleAddTodo}
+                    />
                 </div>
             </div>
         </div>
--- src/components/08-useReducer/todoReducer.js
+++ src/components/08-useReducer/todoReducer.js
@@ -5,30 +5,14 @@ export const todoReducer=(state=[], action)=>{
                 return [...state,action.payload];
         case 'delete':
             return state.filter(todo=>todo.id!==action.payload) 
-            /*EL FILTER REGRESA UN ARREGLO con condiciones 
-            recorrera los todos.id... y eliminara el id en especifico*/
+       
         case 'toogle':// OPTIMO 
             return state.map( todo=>
                 (todo.id===action.payload)
                 ?  {...todo, done: !todo.done}
                 : todo
                 );
-            
-        // case 'toogle-old':// NO OPTIMO :,v
-        //     return state.map(
-        //         todo=>{
-        //             if(todo.id === action.payload){ 
-        //                 return {
-        //                     ...todo,
-        //                     done: !todo.done
-        //                 };
-        //             }else{
-        //                 return todo;
-        //             }
-                
-        //         });
-            /*Permite editar los todos */
-
+             
         default:
             return state;
     }
